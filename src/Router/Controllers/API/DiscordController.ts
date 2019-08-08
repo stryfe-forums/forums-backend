64477e40d24238e09baf1ea880c5a14476bbe20a
Zoro <swftapocalypse1@gmail.com>
2019-08-08 00:41:54 -0400

    I can't think of a commit message.
    Removed: json responses
    Added: api responses now return data within the url queries

--- src/Router/Controllers/API/DiscordController.ts
+++ src/Router/Controllers/API/DiscordController.ts
@@ -1,14 +1,16 @@
-import { Controller, Get } from '@overnightjs/core';
+import { Controller, Get, Middleware } from '@overnightjs/core';
 import { JwtManager } from '@overnightjs/jwt';
 import { Request, Response } from 'express';
-import { OK } from 'http-status-codes';
+import { OK, BAD_REQUEST } from 'http-status-codes';
 
 import fetch from 'node-fetch';
 import btoa from 'btoa';
 import jwt from 'jsonwebtoken';
+import url from 'url';
 
 import { UserAuthData } from '../../../Models';
 import { TypeORMController } from '../../../Database';
+import { JWTBody } from '../../../Interfaces';
 
 @Controller('api/discord')
 export class DiscordAPIController {
@@ -22,7 +24,7 @@ export class DiscordAPIController {
 
 	@Get('login')
 	private getLogin(req: Request, res: Response) {
-		res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=525465644578111498&scope=identify&response_type=code&redirect_uri=${this.redirect}`);
+		return res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=525465644578111498&scope=identify&response_type=code&redirect_uri=${this.redirect}`);
 	}
 
 	@Get('callback')
@@ -30,7 +32,7 @@ export class DiscordAPIController {
 		if (!req.query.code) throw new Error('No code was provided.');
 
 		const code = req.query.code;
-		const creds = btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`);
+		const creds = btoa(process.env.JWT_SECRET);
 
 		const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${this.redirect}`, {
 			method: 'POST',
@@ -41,9 +43,6 @@ export class DiscordAPIController {
 
 		const json = await response.json();
 		const userInfo = await this.getUserInfo(json.access_token);
-		// const entry = await this.database.getModel<UserAuthData>('userAuthData').findOne({ jwtToken: req.query.state });
-
-		// if (entry) return res.status(OK).json({ payload: entry.jwtToken }); // @TODO: this is does nothing as we stopped using this method, so it needs to be fixed
 
 		const payload = this.jwt.jwt({
 			access_token: json.access_token,
@@ -56,16 +55,34 @@ export class DiscordAPIController {
 		data.jwtToken = payload;
 
 		await this.database.getModel<UserAuthData>('userAuthData').save(data);
-
-		return res.status(OK).json({
-			payload, 
-			userInfo: {
-				id: userInfo.id,
-				avatar: userInfo.avatar,
-				username: userInfo.username,
-				flags: userInfo.flags
+		
+		return res.redirect(url.format({ // @TODO: Refactor all the things and make them read from environment variables
+			host: 'http://localhost:3000/api/discord/loggedin',
+			query: {
+				payload,
+				user_id: userInfo.id,
+				user_name: userInfo.username
 			}
-		});
+		}));
+	}
+
+	@Get('loggedin')
+	private async getLoggedIn(req: Request, res: Response) {
+		return res.send('You have been logged in successfully!');
+	}
+
+	@Get('verify')
+	private async getVerify(req: Request, res: Response) {
+		if (!req.query.token) return res.status(BAD_REQUEST).json({ error: 'Missing token Header!' });
+
+		const decoded = jwt.verify(req.query.token! as string, process.env.JWT_SECRET!) as JWTBody;
+
+		const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+		
+		if (decoded.exp < Date.now()) return res.status(OK).redirect(`${fullUrl}?valid=false&reason=${encodeURIComponent('Token expired.')}`);
+
+		return res.status(OK).redirect(`${fullUrl}?valid=true`);
 	}
 
 	private async getUserInfo(accessToken: string) {
