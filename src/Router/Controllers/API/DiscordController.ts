c2de575540544c910465f7a47aaf2364decc8c20
Zoro <swftapocalypse1@gmail.com>
2019-08-07 22:25:25 -0400

    More authorization stuff, and change how we store data.
    We now store the users auth data within a jwt for security reasons. Next we will be adding some security checks to make sure the user hasn't been click-jacked or anything of that nature.

--- src/Router/Controllers/API/DiscordController.ts
+++ src/Router/Controllers/API/DiscordController.ts
@@ -5,6 +5,7 @@ import { OK } from 'http-status-codes';
 
 import fetch from 'node-fetch';
 import btoa from 'btoa';
+import jwt from 'jsonwebtoken';
 
 import { UserAuthData } from '../../../Models';
 import { TypeORMController } from '../../../Database';
@@ -29,7 +30,8 @@ export class DiscordAPIController {
 		if (!req.query.code) throw new Error('No code was provided.');
 
 		const code = req.query.code;
-		const creds = btoa(`525465644578111498:bTvTW2JjCQZZ8ETxLCuH35-HNvgAMUxr`);
+		const creds = btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`);
+
 		const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${this.redirect}`, {
 			method: 'POST',
 			headers: {
@@ -39,20 +41,30 @@ export class DiscordAPIController {
 
 		const json = await response.json();
 		const userInfo = await this.getUserInfo(json.access_token);
-
-		const entry = await this.database.getModel<UserAuthData>('userAuthData').findOne({ access_token: req.query.code });
+		const entry = await this.database.getModel<UserAuthData>('userAuthData').findOne({ jwtToken: req.query.state });
 
 		if (entry) return res.status(OK).json({ payload: entry.jwtToken });
 
-		console.log('token expires in: ' + JSON.stringify(json))
-
 		const payload = this.jwt.jwt({
 			access_token: json.access_token,
-			user_id: userInfo.id
+			user_id: userInfo.id,
+			refresh_token: json.refresh_token
 		});
 
+		const data = new UserAuthData();
+		data.id = userInfo.id;
+		data.jwtToken = payload;
+
+		await this.database.getModel<UserAuthData>('userAuthData').save(data);
+
 		return res.status(OK).json({
-			payload
+			payload, 
+			userInfo: {
+				id: userInfo.id,
+				avatar: userInfo.avatar,
+				username: userInfo.username,
+				flags: userInfo.flags
+			}
 		});
 	}
 
